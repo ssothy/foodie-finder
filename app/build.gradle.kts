f5bb663982a52f876939571aa65f2596fc1de64c
Sami Sothy <ssothy@wgu.edu>
2024-07-17 10:05:20 -0700

    changed applicationid to upload bundle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.foodiefinder"
+        applicationId = "com.smolsashimi.foodiefinder"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
