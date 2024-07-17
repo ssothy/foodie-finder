5d4fd6b3c8d49247a3b8a1ccf93e684b0f5d277d
Sami Sothy <ssothy@wgu.edu>
2024-07-17 10:33:34 -0700

    upticked database again

--- app/src/main/java/com/example/foodiefinder/Database/AppDatabase.java
+++ app/src/main/java/com/example/foodiefinder/Database/AppDatabase.java
@@ -9,7 +9,7 @@ import androidx.room.RoomDatabase;
 import com.example.foodiefinder.DAO.RestaurantDao;
 import com.example.foodiefinder.Entities.Restaurant;
 
-@Database(entities = {Restaurant.class}, version = 9)
+@Database(entities = {Restaurant.class}, version = 10)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract RestaurantDao restaurantDao();
 
